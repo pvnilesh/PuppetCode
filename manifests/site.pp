74f0bb2e2e9c4d3a56a0408d2680485b004e7ade
Nilesh Vijay Padmagiriwar <Nilesh_P@ad.infosys.com>
2018-01-19 16:18:06 +0530

    commit02

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,3 @@
 node default {
-  include os
+  include createfile
 }
---
+++ modules/createfile/manifests/init.pp
@@ -0,0 +1,6 @@
+class createfile {
+  file { "C:\testfile.txt":
+      ensure => file,
+      content => "This is a test file.",
+  }
+}
