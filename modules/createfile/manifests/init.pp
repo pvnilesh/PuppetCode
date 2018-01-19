0523c600b82b170cab7def69c106efb51057c539
Nilesh Vijay Padmagiriwar <Nilesh_P@ad.infosys.com>
2018-01-19 16:43:24 +0530

    commit03

--- modules/createfile/manifests/init.pp
+++ modules/createfile/manifests/init.pp
@@ -1,6 +1,6 @@
 class createfile {
-  file { "C:\testfile.txt":
-      ensure => file,
-      content => "This is a test file.",
+  file { 'C:/testfile.txt':
+      ensure => 'file',
+      content => 'This is a test file.',
   }
 }
