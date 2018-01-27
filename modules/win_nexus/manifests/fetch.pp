5245aa75aa6a1df047611c80dd8cfbac2a49e5ee
nilesh <nilesh@abc.com>
2018-01-27 17:42:37 +0530

    changed win_nexus::fetch

--- modules/win_nexus/manifests/fetch.pp
+++ modules/win_nexus/manifests/fetch.pp
@@ -11,6 +11,7 @@ class win_nexus::fetch (
   $target_folder,
 ) {
   exec { 'fetch':
-    command => "C:\\windows\\system32\\cmd.exe /c \"python scripts\\fetchArtifact.py -g ${group_id} -a ${artifact_id} -r ${repository} -e ${extension} -n ${nexus_url} -t ${target_folder} -v ${version}\""
+    command => "python scripts\\fetchArtifact.py -g ${group_id} -a ${artifact_id} -r ${repository} -e ${extension} -n ${nexus_url} -t ${target_folder} -v ${version}",
+    path => "C:\\Users\\susanta_aec\\AppData\\Local\\Programs\\Python\\Python36-32",
   }
 }
